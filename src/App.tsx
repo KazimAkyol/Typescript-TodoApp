840734208b5537f0bce214ac98c50cfdf8abec0e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-04 23:48:37 +0100

    add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Container } from "@mui/material";
-
+import Home from "./pages/Home";
 
 function App() {
- 
   return (
     <Container>
-        <h1>hi there</h1>
+      <h1>hi there</h1>
+      <Home />
     </Container>
-   
-
   );
 }
 
