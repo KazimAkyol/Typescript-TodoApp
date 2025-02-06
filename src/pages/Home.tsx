b249b971e64b96f9acbcd0a54ee033900eb995b5
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-06 10:31:21 +0100

    7.commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
 import { Container } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import AddTodo from "../components/AddTodo";
+import axios from "axios";
+
+const url = "https://64ecd95ff9b2b70f2bfb0943.mockapi.io/todos";
+
+interface ITodo {
+  task: string;
+  isDone: boolean;
+  id: string;
+}
 
 export default function Home() {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const getTodos = () => {
+    const {data} = await axios(url);
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <Container>
       <Header />
