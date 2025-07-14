c9097064cee1edfdca8af626ec21a5a8984c4e1b
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-14 13:37:51 +0200

    added swertAlert.ts

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,25 @@
+import { DeleteOutline } from '@mui/icons-material'
+import { IconButton, ListItem, ListItemText } from '@mui/material'
+import React from 'react'
+
+interface ITodoItemProps {
+    todo: ITodo
+    toggleTodo: ToggleFn
+    deleteTodo: DeleteFn
+}
+
+export default function TodoItem({ todo, toggleTodo, deleteTodo }: ITodoItemProps) {
+    return (
+        <ListItem
+            disableGutters
+            sx={{ padding: "1rem", cursor: "pointer" }}
+            secondaryAction={
+                <IconButton aria-label="comment">
+                    <DeleteOutline sx={{ "&:hover": { color: "red" } }} onClick={() => deleteTodo(todo.id)} />
+                </IconButton>
+            }
+        >
+            <ListItemText onClick={() => toggleTodo(todo)} primary={todo.task} />
+        </ListItem>
+    )
+}
